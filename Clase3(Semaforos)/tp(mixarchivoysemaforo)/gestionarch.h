6c2f911a617f79a2c88011e097c0f3aacdc1965d
AgustinIgnacioVazquez <aguvazquez%40frba.utn.edu.ar>
2023-09-11 20:12:42 -0300

    framework working

---
+++ Clase3(Semaforos)/tp(mixarchivoysemaforo)/gestionarch.h
@@ -0,0 +1,12 @@
+#ifndef _GESTIONARCH
+#define _GESTIONARCH
+
+FILE * AbrirArchivo(FILE *, char *);
+
+void voLeerArchivo(FILE *);
+
+void voEscribirArchivo(FILE *, const char[]);
+
+void voCerrarArchivo(FILE *);
+
+#endif
---
+++ Framework/gestionarch.c
@@ -0,0 +1,41 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+#include <unistd.h>
+#include <gestionarch.h>
+
+char read_buffer[20 + 1];
+
+FILE * AbrirArchivo(FILE *archivo, char modo[]){
+    
+    // Abrir el archivo en modo escritura
+    archivo = fopen("productos.txt", modo);
+
+    // Verificar si el archivo se abrió correctamente
+    if (archivo == NULL) {
+        printf("No se pudo abrir el archivo.\n");
+        return NULL;  // Terminar el programa con código de error
+    }else{
+        return archivo;
+    }
+}
+
+void voLeerArchivo(FILE *archivo){
+    printf("El contenido del archivo es: ");
+    // Abrir archivo en modo lectura
+    archivo = fopen("productos.txt", "r");
+
+    while (!feof(archivo))
+    {
+        fscanf(archivo, "%s ", read_buffer);
+        printf("%s\n", read_buffer);
+    }
+}
+
+void voEscribirArchivo(FILE *archivo, const char contenido[]){
+    fprintf(archivo, "%s", contenido);
+}
+
+void voCerrarArchivo(FILE *archivo){
+    fclose(archivo);
+}
