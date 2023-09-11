ccf469ff558854d6242b984dd7c95d32bee5c5bf
AgustinIgnacioVazquez <aguvazquez%40frba.utn.edu.ar>
2023-09-11 19:11:11 -0300

    folder rename

--- Clase3(Semaforos)/main2.c
+++ Clase3(Semaforos)/main2.c
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <clave.h>
 #include <semaforo.h>
 
 int main(int arg, char *argv[])
 {
+    int i;
     int id_semaforo;
 
     id_semaforo = creo_semaforo();
 
-    inicia_semaforo(id_semaforo, VERDE);
-
     while (1)
     {
         espera_semaforo(id_semaforo);
-        printf("Seccion critica!\n");
+        printf("Seccion critica2!\n");
         sleep(1);
         levanta_semaforo(id_semaforo);
         sleep(10);
