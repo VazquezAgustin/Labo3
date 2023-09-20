22ef3de7254212e76a7575a67512ec7fb970e9f1
AgustinIgnacioVazquez <aguvazquez%40frba.utn.edu.ar>
2023-09-20 20:35:08 -0300

    hasta aca funciona

--- PrimerParcial/alumno.c
+++ PrimerParcial/alumno.c
@@ -9,8 +9,18 @@
 
     int main(int arg, char *argv[])
     {
+        //semaforos y archivos
+        FILE *archivo_origen = NULL;
+        FILE *archivo_destino = NULL;
+        char nombre_archivo_destino[15];
+        int id_semaforo;
+        char contenido_archivo[20 + 1];
 
+        //otros
         char tipo_alumno;
+        char letra_tipo_alumno[2];
+        char *linea;
+        char *token;
 
         if (arg != 2) {
             printf("Uso: Ingresar 1 o 2 como parametro de entrada (1 para Inicial / 2 para provenientes de otra universidad) \n");
@@ -19,5 +29,58 @@
 
         tipo_alumno = atoi(argv[1]);
 
+        memset(letra_tipo_alumno, 0x00, sizeof(letra_tipo_alumno));
+        if (tipo_alumno == 1)
+        {
+            // Alumno inicial sin materias
+            printf("Se inicia el proceso para I \n");
+            memcpy(nombre_archivo_destino, "iniciales.dat", 13);
+            memcpy(letra_tipo_alumno, "I", 1);
+            printf("nombre archivo destino %s \n", nombre_archivo_destino);
+            printf("letra tipo alumno %s \n", letra_tipo_alumno);
+        }
+        if (tipo_alumno == 2)
+        {
+            // Alumno de otra universidad con materias
+            printf("Se inicia el proceso para M \n");
+            memcpy(nombre_archivo_destino, "materiasp.dat", 13);
+            memcpy(letra_tipo_alumno, "M", 1);
+            printf("nombre archivo destino %s \n", nombre_archivo_destino);
+            printf("letra tipo alumno %s \n", letra_tipo_alumno);
+
+        }
+
+        id_semaforo = creo_semaforo(); 
+
+        while(1)
+        {
+            // leo de origen y verifico si este proceso debe procesarlo
+            espera_semaforo(id_semaforo);
+            archivo_origen = AbrirArchivo(archivo_origen, "r", "cargas.dat");
+            linea = leerPrimeraLinea(archivo_origen);
+            voCerrarArchivo(archivo_origen);
+            levanta_semaforo(id_semaforo);
+
+            if (linea != NULL)
+            {   
+
+                // envio a destino correspondiente
+                espera_semaforo(id_semaforo);
+                archivo_destino = AbrirArchivo(archivo_destino, "a", nombre_archivo_destino);
+                memset(contenido_archivo, 0x00, sizeof(contenido_archivo));
+                snprintf(contenido_archivo, 20, "%s", linea);
+                voEscribirArchivo(archivo_destino, contenido_archivo);
+                
+                // reinicio archivo
+                archivo_origen = AbrirArchivo(archivo_origen, "w", "cargas.dat");
+
+                voCerrarArchivo(archivo_origen);
+                voCerrarArchivo(archivo_destino);
+                levanta_semaforo(id_semaforo);
+                
+            }
+            
+            usleep(500 * 1000);
+        }
         return 0;
     }
--- PrimerParcial/main1.c
+++ PrimerParcial/main1.c
@@ -8,43 +8,71 @@
 
 int main(int arg, char *argv[])
 {
+    //archivos y semaforo
+    int id_semaforo = 0;
+    char contenido_archivo[20 + 1];
+    FILE *archivo = NULL;
+
+    // otras
     int menu_deseado = 0;
     char tipo_alumno[2];
     int cantidad_materias = 0;
     char nombre_alumno[30];
     char apellido_alumno[30];
 
+    id_semaforo = creo_semaforo();
+    inicia_semaforo(id_semaforo, VERDE);
 
-    printf("Ingrese menu deseado (1 para carga alumnos/2 para leer inscriptos) \n");
-    scanf("%d", &menu_deseado);
-    
-    if (menu_deseado == 1)
+    // creando archivo vacio
+    archivo = AbrirArchivo(archivo, "w", "cargas.dat");
+    voCerrarArchivo(archivo);
+
+    while (1)
     {
-        // MENU 1
-        printf("Tipo de alumno: (I para inicial/M para materias a validar) \n");
-        scanf("%c", tipo_alumno);
 
-        if(strcmp(tipo_alumno, "M") == 0){
-            printf("Ingrese la cantidad de materias: \n");
-            scanf("%d", &cantidad_materias);
+        printf("Ingrese menu deseado (1 para carga alumnos/2 para leer inscriptos) \n");
+        scanf("%d", &menu_deseado);
+        
+        if (menu_deseado == 1)
+        {
+            // MENU 1
+            printf("Tipo de alumno: (I para inicial/M para materias a validar) \n");
+            memset(tipo_alumno, 0x00, sizeof(tipo_alumno));
+            scanf("%s", tipo_alumno);
+
+            if(strcmp(tipo_alumno, "M") == 0){
+                printf("Ingrese la cantidad de materias: \n");
+                scanf("%d", &cantidad_materias);
+            }
+
+            printf("Ingrese nombre del alumno \n");
+            memset(nombre_alumno, 0x00, sizeof(nombre_alumno));
+            scanf("%s", nombre_alumno);
+
+            printf("Ingrese apellido del alumno \n");
+            memset(apellido_alumno, 0x00, sizeof(apellido_alumno));
+            scanf("%s", apellido_alumno);
+
+            // enviar a archivo
+            espera_semaforo(id_semaforo);
+            archivo = AbrirArchivo(archivo, "w", "cargas.dat");
+
+            memset(contenido_archivo, 0x00, sizeof(contenido_archivo));
+            snprintf(contenido_archivo, 20, "%s,%d,%s,%s", tipo_alumno, cantidad_materias, nombre_alumno, apellido_alumno);
+            voEscribirArchivo(archivo, contenido_archivo);
+
+            voCerrarArchivo(archivo);
+            levanta_semaforo(id_semaforo);
         }
-
-        printf("Ingrese nombre del alumno \n");
-        memset(nombre_alumno, 0x00, sizeof(nombre_alumno));
-        scanf("%s", nombre_alumno);
-
-        printf("Ingrese apellido del alumno \n");
-        memset(apellido_alumno, 0x00, sizeof(apellido_alumno));
-        scanf("%s", apellido_alumno);
+        
+        if (menu_deseado == 2)
+        {
+            // MENU 2
+            printf("Tipo de alumno a leer: (I para inicial/M para materias a validar) \n");
+            memset(tipo_alumno, 0x00, sizeof(tipo_alumno));
+            scanf("%s", tipo_alumno);
+        }
     }
-    
-    if (menu_deseado == 2)
-    {
-        // MENU 2
-        printf("Tipo de alumno a leer: (I para inicial/M para materias a validar) \n");
-        scanf("%c", tipo_alumno);
-    }
-    
 
     return 0;
 }
